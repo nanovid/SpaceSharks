0bf7255cf8a30328d281c1e8c9c27cee391daf2f
Nicholas Haycocks-Tulli <Nicholas>
2025-10-04 12:54:05 -0400

    First commit to my thing

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from scipy.optimize import minimize
 
-
 A = np.array([[0, 1], [-2, -3]])
 print(A)
-=======
-print("something")
->>>>>>> 269186a8922edf7eb383ea22f16d37bb85552768
