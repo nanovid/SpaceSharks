0280c6917d3301f04daee1293d697a2548e39366
Anish <a2chocka@uwaterloo.ca>
2025-10-04 12:52:06 -0400

    test

--- Untitled-1.py
+++ Untitled-1.py
@@ -7,6 +7,7 @@ from scipy.optimize import minimize
 
 A = np.array([[0, 1], [-2, -3]])
 print(A)
-=======
-print("something")
->>>>>>> 269186a8922edf7eb383ea22f16d37bb85552768
+
+def system_dynamics(t, x):
+    return A @ x
+
